1679da07179032654d5eb95e532b02499e049a49
sandra juarez <drasan_mar@hotmail.com>
2017-06-05 15:55:37 -0500

    Formularios con campos faltantes

--- app/scripts/controllers/solicitudes.js
+++ app/scripts/controllers/solicitudes.js
@@ -9,27 +9,5 @@
  */
 angular.module('sicamsaApp')
   .controller('SolicitudesCtrl', function ($scope) {
-    $scope.events = [{
-      d: new Date(),
-      text: 'ejemplo',
-      color: '#6e7f29'
-    }];
-
-    $scope.settings = {
-      theme: 'mobiscroll',
-      display: 'inline',
-      layout: 'liquid',
-      calendarScroll: 'horizontal',
-      showOuterDays: true,
-      showEventCount: true,
-      lang: 'es',
-      eventText: "feriado",
-      eventsText: "feriados"
-    };
-
-    $scope.settingsInput = {
-      theme: 'mobiscroll',
-      display: 'bubble',
-      lang: 'es',
-    };
+ 
   });
