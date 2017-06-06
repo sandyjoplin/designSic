addcd3b80aa39f9d27b8553103b43d3e36552029
sandra juarez <drasan_mar@hotmail.com>
2017-06-05 23:32:51 -0500

    Terminando area de solicitudes de permiso

--- app/scripts/controllers/solicitudes.js
+++ app/scripts/controllers/solicitudes.js
@@ -9,5 +9,15 @@
  */
 angular.module('sicamsaApp')
   .controller('SolicitudesCtrl', function ($scope) {
- 
+  $scope.settingsInput = {
+            theme: 'mobiscroll',
+            display: 'bubble',
+            lang: 'es',
+        };
+        $scope.settingsSelect = {
+            theme: 'mobiscroll',
+            display: 'bubble',
+            minWidth: 200,
+            lang: 'es'
+        };
   });
