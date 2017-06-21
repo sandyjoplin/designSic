58c5a76f20464f76de829803c79e9eb2dcca654e
sandra juarez <drasan_mar@hotmail.com>
2017-06-20 21:19:18 -0500

    contro de automoviles

--- app/scripts/controllers/verificacionunidades.js
+++ app/scripts/controllers/verificacionunidades.js
@@ -9,6 +9,23 @@
  */
 angular.module('sicamsaApp')
   .controller('VerificacionunidadesCtrl', function ($scope) {
+     $scope.settingsInput = {
+            theme: 'mobiscroll',
+            display: 'bubble',
+            lang: 'es',
+        };
+        $scope.settingsSelect = {
+            theme: 'mobiscroll',
+            display: 'bubble',
+            minWidth: 200,
+            lang: 'es'
+        };
+         $scope.settingsDate = {
+            theme: 'mobiscroll',
+            display: 'bubble',
+            maxWidth: 200,
+            lang: 'es'
+        };
     $scope.tabUno = true;
     $scope.tabdos = false;
     $scope.cambiarTabUno = function () {
