05a527d89689f8efd00c35121b5ec5ea4ecf2207
sandra juarez <drasan_mar@hotmail.com>
2017-06-29 13:06:42 -0500

    cambios de jaime

--- app/scripts/controllers/verificacionunidades.js
+++ app/scripts/controllers/verificacionunidades.js
@@ -9,27 +9,63 @@
  */
 angular.module('sicamsaApp')
   .controller('VerificacionunidadesCtrl', function ($scope) {
-     $scope.settingsInput = {
-            theme: 'mobiscroll',
-            display: 'bubble',
-            lang: 'es',
-        };
-        $scope.settingsSelect = {
-            theme: 'mobiscroll',
-            display: 'bubble',
-            minWidth: 200,
-            lang: 'es'
-        };
-         $scope.settingsDate = {
-            theme: 'mobiscroll',
-            display: 'bubble',
-            maxWidth: 200,
-            lang: 'es'
-        };
+    $scope.settingsInput = {
+      theme: 'mobiscroll',
+      display: 'bubble',
+      lang: 'es',
+    };
+    $scope.settingsSelect = {
+      theme: 'mobiscroll',
+      display: 'bubble',
+      minWidth: 200,
+      lang: 'es'
+    };
+    $scope.settingsDate = {
+      theme: 'mobiscroll',
+      display: 'bubble',
+      maxWidth: 200,
+      lang: 'es'
+    };
     $scope.tabUno = true;
     $scope.tabdos = false;
     $scope.cambiarTabUno = function () {
       $scope.tabUno = !$scope.tabUno;
       $scope.tabdos = !$scope.tabdos;
     }
+
+
+
+
+
+
+
+    $scope.tabU = true;
+    $scope.tabD = false;
+    $scope.tabT = false;
+    $scope.tabCu = false;
+    $scope.tabUnofn = function () {
+      $scope.tabU = true;
+      $scope.tabD = false;
+      $scope.tabT = false;
+      $scope.tabCu = false;
+    }
+    $scope.tabDosfn = function () {
+      $scope.tabU = false;
+      $scope.tabD = true;
+      $scope.tabT = false;
+      $scope.tabCu = false;
+    }
+    $scope.tabTresfn = function () {
+      $scope.tabU = false;
+      $scope.tabD = false;
+      $scope.tabT = true;
+      $scope.tabCu = false;
+    }
+    $scope.tabCuatrofn = function () {
+      $scope.tabU = false;
+      $scope.tabD = false;
+      $scope.tabT = false;
+      $scope.tabCu = true;
+    }
+    
   });
